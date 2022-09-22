dc0dff31b4377c7173c724b022341138d596403d
timdoan22 <timdoan01@gmail.com>
2022-09-22 16:22:11 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>
+        <h1>My Favourite Fruits</h1>
+        <ul>
+            <li>Mangoes</li>
+            <li>Bananas</li>
+            <li>Golden Kiwi</li>
+        </ul>
+    </div>, 
+    document.getElementById("root")
+);
